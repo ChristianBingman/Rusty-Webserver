d28fbf53c28dbc38158d448bbb713d4ab2670647
ChristianBingman <christianbingman@gmail.com>
2024-08-17 22:35:58 -0500

    More significant refactor
    Broke the threaded and threadpooled server modes, but you can have any
    request handler function. There is also still not a clear way to send
    the opts between calls.

--- src/file.rs
+++ src/file.rs
@@ -1,16 +1,15 @@
 use core::str;
-use std::{fs, io};
-use std::path::Path;
 use std::ffi::OsStr;
+use std::path::Path;
+use std::{fs, io};
 
 use crate::http10::content_types::get_mime;
 
-const TRYFILES: [&'static str; 2] = [ "/index.html", "/index.htm" ];
-const BASE_DIR: &'static str = "./";
+const TRYFILES: [&'static str; 2] = ["/index.html", "/index.htm"];
 
 #[derive(Debug)]
 pub enum FileError {
-    ReadError(io::Error)
+    ReadError(io::Error),
 }
 
 #[derive(Debug)]
@@ -30,8 +29,8 @@ impl std::fmt::Display for File {
 }
 
 impl File {
-    pub fn try_load(uri: String) -> Result<Self, FileError>{
-        let path = Path::new(BASE_DIR).join(&uri[1..]);
+    pub fn try_load(uri: String, base_dir: &str) -> Result<Self, FileError> {
+        let path = Path::new(base_dir).join(&uri[1..]);
         if let Ok(exists) = path.try_exists() {
             if !exists {
                 return Err(FileError::ReadError(io::ErrorKind::NotFound.into()));
@@ -40,24 +39,40 @@ impl File {
             return Err(FileError::ReadError(io::ErrorKind::NotFound.into()));
         }
         if path.is_dir() {
-            let try_files: Vec<Result<Self, FileError>> = TRYFILES.iter().map(|file| Self::try_load(Path::new(&uri).join(file).to_str().unwrap().to_string())).collect();
+            let try_files: Vec<Result<Self, FileError>> = TRYFILES
+                .iter()
+                .map(|file| {
+                    Self::try_load(
+                        Path::new(&uri).join(file).to_str().unwrap().to_string(),
+                        base_dir,
+                    )
+                })
+                .collect();
             if let Some(file) = try_files.into_iter().find_map(Result::ok) {
                 return Ok(file);
             } else {
                 return Err(FileError::ReadError(io::ErrorKind::NotFound.into()));
             }
         }
-        let extension: Option<String> = path.extension().and_then(OsStr::to_str).map(|ext| ext.to_string());
+        let extension: Option<String> = path
+            .extension()
+            .and_then(OsStr::to_str)
+            .map(|ext| ext.to_string());
         let mime_type = get_mime(extension.clone().unwrap_or("".to_string())).to_string();
         let content: Result<Vec<u8>, std::io::Error> = fs::read(&path);
 
         if content.is_ok() {
             let size = content.as_ref().unwrap().len();
-            Ok(File { path: path.to_str().unwrap().to_string(), extension, content: content.unwrap(), mime_type, size })
+            Ok(File {
+                path: path.to_str().unwrap().to_string(),
+                extension,
+                content: content.unwrap(),
+                mime_type,
+                size,
+            })
         } else {
             Err(FileError::ReadError(content.unwrap_err()))
         }
-
     }
 
     pub fn get_content(&self) -> Vec<u8> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,227 @@
-pub mod http10;
 mod file;
-pub mod threadpool;
-use core::str;
-use std::{io::{Read, Write}, net::TcpStream};
+mod http10;
+mod threadpool;
 
-use http10::{handle_request, request::HTTPRequest};
+#[derive(Debug, PartialEq)]
+pub struct Opts {
+    /// port to bind to
+    port: u16,
 
-pub fn handle_client(mut stream: TcpStream) {
-    let local: String = match stream.local_addr() {
-        Ok(addr) => addr.to_string(),
-        Err(_) => "Invalid Address".to_string(),
-    };
-    let remote: String = match stream.peer_addr() {
-        Ok(addr) => addr.to_string(),
-        Err(_) => "Invalid Address".to_string(),
-    };
-    let mut request: Vec<u8> = Vec::new();
-    let mut buf = [0u8; 4096];
-    log::info!("Received connection from {} to {}", remote, local);
-    while HTTPRequest::try_from(&request).is_err() {
-        match stream.read(&mut buf) {
-            Ok(0) => break,
-            Ok(n) => {
-                if let Ok(parsed_data) = str::from_utf8(&buf[..n]) {
-                    log::info!("Received: {}", parsed_data);
-                    request.append(buf[..n].to_vec().as_mut());
-                } else {
-                    request.append(buf[..n].to_vec().as_mut());
-                }
-            },
-            Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => (),
-            Err(_) => break
+    /// address to bind to
+    bind: String,
+
+    /// directory to serve
+    directory: String,
+
+    /// protocol to use (supports http 1.0)
+    protocol: String,
+}
+
+impl Default for Opts {
+    fn default() -> Self {
+        Opts {
+            port: 8080,
+            bind: "127.0.0.1".to_string(),
+            directory: "./".to_string(),
+            protocol: "HTTP/1.0".to_string(),
         }
     }
-    stream.write_all(handle_request(request).as_bytes().as_slice()).unwrap();
+}
+
+pub mod http_server {
+    use core::str;
+    use std::io::{Read, Write};
+    use std::net::{TcpListener, TcpStream};
+
+    use chrono::Utc;
+
+    use crate::file::{File, FileError};
+    use crate::http10::headers::Header;
+    use crate::http10::methods::Method;
+    use crate::http10::result_codes::ResultCode;
+    use crate::http10::{request::HTTPRequest, response::HTTPResponse};
+
+    use super::Opts;
+
+    #[derive(Debug, PartialEq)]
+    pub enum HTTPServerClass {
+        Simple,
+        Threaded,
+        ThreadPooled,
+    }
+
+    pub struct HTTPServer {
+        class: HTTPServerClass,
+        args: Opts,
+        handler: Box<dyn Fn(TcpStream) -> () + Send + 'static>,
+    }
+
+    impl HTTPServer {
+        fn default_handler(req: HTTPRequest) -> HTTPResponse {
+            let mut headers: Vec<Header> = vec![
+                Header::Date(Utc::now().into()),
+                Header::Server("Rusty Webserver".to_string()),
+            ];
+
+            match req.method {
+                Method::GET => {
+                    let f = File::try_load(req.uri, "./");
+                    match f {
+                        Ok(file) => {
+                            headers.push(Header::ContentType(file.get_mime()));
+                            headers.push(Header::ContentLength(file.get_size()));
+                            HTTPResponse::new(
+                                "HTTP/1.0".to_string(),
+                                ResultCode::OK,
+                                headers,
+                                Some(file.get_content()),
+                            )
+                        }
+                        Err(err) => match err {
+                            FileError::ReadError(err)
+                                if err.kind() == std::io::ErrorKind::NotFound =>
+                            {
+                                headers.push(Header::ContentType("text/plain".to_string()));
+                                HTTPResponse::new(
+                                    "HTTP/1.0".to_string(),
+                                    ResultCode::NotFound,
+                                    headers,
+                                    Some(
+                                        Into::<String>::into(ResultCode::NotFound)
+                                            .as_bytes()
+                                            .to_vec(),
+                                    ),
+                                )
+                            }
+                            _ => {
+                                headers.push(Header::ContentType("text/plain".to_string()));
+                                HTTPResponse::new(
+                                    "HTTP/1.0".to_string(),
+                                    ResultCode::InternalServerError,
+                                    headers,
+                                    Some(
+                                        Into::<String>::into(ResultCode::InternalServerError)
+                                            .as_bytes()
+                                            .to_vec(),
+                                    ),
+                                )
+                            }
+                        },
+                    }
+                }
+                Method::HEAD => {
+                    HTTPResponse::new("HTTP/1.0".to_string(), ResultCode::OK, headers, None)
+                }
+                Method::POST => {
+                    log::info!(
+                        "Received POST Data {:?}",
+                        str::from_utf8(req.body.unwrap().as_ref())
+                    );
+                    headers.push(Header::ContentType("text/plain".to_string()));
+                    HTTPResponse::new(
+                        "HTTP/1.0".to_string(),
+                        ResultCode::NotImplemented,
+                        headers,
+                        Some(
+                            Into::<String>::into(ResultCode::NotImplemented)
+                                .as_bytes()
+                                .to_vec(),
+                        ),
+                    )
+                }
+            }
+        }
+
+        fn handle_stream(
+            mut stream: TcpStream,
+            handler: &Box<dyn Fn(HTTPRequest) -> HTTPResponse + Send + 'static>,
+        ) {
+            let local: String = match stream.local_addr() {
+                Ok(addr) => addr.to_string(),
+                Err(_) => "Invalid Address".to_string(),
+            };
+            let remote: String = match stream.peer_addr() {
+                Ok(addr) => addr.to_string(),
+                Err(_) => "Invalid Address".to_string(),
+            };
+            let mut request: Vec<u8> = Vec::new();
+            let mut buf = [0u8; 4096];
+            log::info!("Received connection from {} to {}", remote, local);
+            while HTTPRequest::try_from(&request).is_err() {
+                match stream.read(&mut buf) {
+                    Ok(0) => break,
+                    Ok(n) => {
+                        if let Ok(parsed_data) = str::from_utf8(&buf[..n]) {
+                            log::info!("Received: {}", parsed_data);
+                            request.append(buf[..n].to_vec().as_mut());
+                        } else {
+                            request.append(buf[..n].to_vec().as_mut());
+                        }
+                    }
+                    Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => (),
+                    Err(_) => break,
+                }
+            }
+            let request = HTTPRequest::try_from(&request).unwrap();
+            stream
+                .write_all(handler(request).as_bytes().as_slice())
+                .unwrap();
+        }
+
+        pub fn new(
+            class: HTTPServerClass,
+            opts: Opts,
+            handler: Option<Box<dyn Fn(HTTPRequest) -> HTTPResponse + Send + 'static>>,
+        ) -> HTTPServer {
+            let handler = match handler {
+                Some(handl) => handl,
+                None => Box::new(HTTPServer::default_handler)
+                    as Box<dyn Fn(HTTPRequest) -> HTTPResponse + Send + 'static>,
+            };
+
+            let handler = Box::new(move |stream| {
+                HTTPServer::handle_stream(stream, &handler);
+            });
+            HTTPServer {
+                class,
+                args: opts,
+                handler,
+            }
+        }
+
+        pub fn serve_forever(self) {
+            match self.class {
+                HTTPServerClass::Simple => {
+                    let listener =
+                        TcpListener::bind(format!("{}:{}", self.args.bind, self.args.port))
+                            .expect("Unable to bind!");
+
+                    for stream in listener.incoming() {
+                        (*self.handler)(stream.expect("Test"));
+                    }
+                }
+                _ => unimplemented!(),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_create_single_threaded_server() {
+            HTTPServer::new(HTTPServerClass::Simple, Opts::default(), None);
+        }
+
+        #[test]
+        fn test_create_threaded_server() {
+            HTTPServer::new(HTTPServerClass::Threaded, Opts::default(), None);
+        }
+
+        #[test]
+        fn test_create_threadpool_server() {
+            HTTPServer::new(HTTPServerClass::ThreadPooled, Opts::default(), None);
+        }
+    }
 }
